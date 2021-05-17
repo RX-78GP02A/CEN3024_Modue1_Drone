510dab0f3fe2505353470efd5dc4c0b19cae8f17
RX-78GP02A <mridley@knights.ucf.edu>
2021-05-16 22:38:36 -0400

    created

---
+++ Ridley_Drone.java
@@ -0,0 +1,21 @@
+// Import  Scanner class
+import java.util.Scanner;
+
+    // Declaration of Ridley_Drone class
+    public class Ridley_Drone {
+
+        // Declaration of Drone class with movement variables
+        public static class Drone {
+            // x variable handles up and down movement
+            private int x;   //forward backward
+            // y variable handles forward and backward movement
+            private int y;   //right left
+            // z variable handles right and left movement
+            private int z;   //up down
+
+            // Constructor
+            public Drone() {
+                x = 0;
+                y = 0;
+                z = 0;
+            }
